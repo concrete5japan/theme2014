a479d5b98e8b251610570bee72f1d52004015f6f
hissy <hishikawa@concrete5.co.jp>
2014-04-09 19:48:40 +0900

    Theme tweaks

--- themes/c5japan2014/elements/footer.php
+++ themes/c5japan2014/elements/footer.php
@@ -5,7 +5,7 @@
 			<div class="col-md-4">
 				<nav class="footer-nav">
 					<?php
-						$a = new GlobalArea('footer1');
+						$a = new GlobalArea('Footer Col Left');
 						$a->display();
 					?>
 				</nav>
@@ -13,7 +13,7 @@
 			<div class="col-md-4">
 				<nav class="footer-nav">
 					<?php
-						$a = new GlobalArea('footer2');
+						$a = new GlobalArea('Footer Col Center');
 						$a->display();
 					?>
 				</nav>
@@ -21,7 +21,7 @@
 			<div class="col-md-4">
 				<nav class="footer-nav">
 					<?php
-						$a = new GlobalArea('footer3');
+						$a = new GlobalArea('Footer Col Right');
 						$a->display();
 					?>
 				</nav>
@@ -30,7 +30,7 @@
 	</div><!--container-->
 	<div class="container">
 		<footer>
-			<p>© 2009-2014 <a href="/">Concrete5 Japan Users Group</a> &amp; <a href="http://concrete5.org" target="_blank">Concrete CMS, Inc.</a>.<br>
+			<p>&copy; 2009-<?php echo date('Y'); ?> <a href="/">Concrete5 Japan Users Group</a> &amp; <a href="http://concrete5.org" target="_blank">Concrete CMS, Inc.</a>.<br>
 			concrete5はMITライセンスで配布されているオープンソースCMSです。<br>
 			"concrete5"のロゴならびに名称は、<a href="http://concrete5.org/" target="_blank">Concrete CMS, Inc.</a>と<a href="http://concrete5.co.jp/" target="_blank">コンクリートファイブジャパン株式会社</a>の登録商標です。<a href="/about/use_of_concrete5_logo/">名称とロゴの使用について</a>。<br>
 			All rights reserved. サイト内の情報の著作権は放棄しておりません。</p>
