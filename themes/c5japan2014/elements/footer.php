74ce5126be54c58447bf821d3dcb4497acb6574f
Katz Ueno <katz@concrete5.co.jp>
2014-11-05 09:13:09 +0900

    Copyright to PortlandLabs

--- themes/c5japan2014/elements/footer.php
+++ themes/c5japan2014/elements/footer.php
@@ -30,9 +30,9 @@
 	</div><!--container-->
 	<div class="container">
 		<footer>
-			<p>&copy; 2009-<?php echo date('Y'); ?> <a href="/">Concrete5 Japan Users Group</a> &amp; <a href="http://concrete5.org" target="_blank">Concrete CMS, Inc.</a>.<br>
+			<p>&copy; 2009-<?php echo date('Y'); ?> <a href="/">concrete5 Japan Users Group</a> &amp; <a href="http://concrete5.org" target="_blank">PortlandLabs, Inc.</a>.<br>
 			concrete5はMITライセンスで配布されているオープンソースCMSです。<br>
-			"concrete5"のロゴならびに名称は、<a href="http://concrete5.org/" target="_blank">Concrete CMS, Inc.</a>と<a href="http://concrete5.co.jp/" target="_blank">コンクリートファイブジャパン株式会社</a>の登録商標です。<a href="/about/use_of_concrete5_logo/">名称とロゴの使用について</a>。<br>
+			"concrete5"のロゴならびに名称は、<a href="http://concrete5.org/" target="_blank">PortlandLabs, Inc.</a>と<a href="http://concrete5.co.jp/" target="_blank">コンクリートファイブジャパン株式会社</a>の登録商標です。<a href="/about/use_of_concrete5_logo/">名称とロゴの使用について</a>。<br>
 			All rights reserved. サイト内の情報の著作権は放棄しておりません。</p>
 		</footer>
 	</div><!--container-->
